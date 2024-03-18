f321f5967b7a5999d5b17202d52dc52f52eff214
JeongwooHam <hamjw0122@gmail.com>
2024-03-18 23:07:48 +0900

    feat: prettier, eslint 설정

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,30 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
-  },
+	plugins: ['react', 'simple-import-sort', 'prettier', '@typescript-eslint'],
+	ignorePatterns: ['vite.config.ts', 'vite-env.d.ts'],
+	rules: {
+		'simple-import-sort/imports': 'error',
+		'simple-import-sort/exports': 'error',
+		'prettier/prettier': ['error'],
+		'no-unused-vars': 'error',
+		'prefer-const': 'error',
+		'no-irregular-whitespace': 'error',
+		'no-trailing-spaces': 'error',
+		'no-empty-function': 'error',
+		'no-duplicate-imports': 'error',
+		'newline-after-var': 'error',
+		'prettier/prettier': ['error', { endOfLine: 'auto' }],
+	},
+	overrides: [
+		{
+			files: ['**/*.ts?(x)'],
+			parser: '@typescript-eslint/parser',
+		},
+		{
+			files: ['**/*.js?(x)'],
+			parserOptions: {
+				sourceType: 'module',
+				ecmaVersion: 2015,
+			},
+		},
+	],
 }
