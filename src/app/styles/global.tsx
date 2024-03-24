5ce037d55c7b8534c7a74b74ab1ce46e80e79f63
JeongwooHam <hamjw0122@gmail.com>
2024-03-25 04:06:34 +0900

    style: 로그인 페이지 퍼블리싱

--- src/app/styles/global.tsx
+++ src/app/styles/global.tsx
@@ -1,21 +1,38 @@
+import { extendTheme } from '@chakra-ui/react'
 import { css, Global } from '@emotion/react'
 
 import { PALETTE } from './theme'
 
+const globalStyle = css`
+	body {
+		font-family: 'Noto Sans KR', sans-serif;
+		background-color: ${PALETTE['dark-600']};
+		color: white;
+	}
+
+	ul {
+		list-style: none;
+	}
+`
+
 const GlobalStyles = () => {
-	const globalStyle = css`
-		body {
-			font-family: 'Noto Sans KR', sans-serif;
-			background-color: ${PALETTE['dark-600']};
-			color: white;
-		}
-
-		ul {
-			list-style: none;
-		}
-	`
-
 	return <Global styles={globalStyle} />
 }
+
+export const ChakraGlobalStyles = extendTheme({
+	styles: {
+		global: {
+			html: { height: 'full' },
+			body: {
+				fontFamily: 'Noto Sans KR, sans-serif',
+				backgroundColor: `${PALETTE['dark-600']}`,
+				color: 'white',
+			},
+			ul: {
+				listStyle: 'none',
+			},
+		},
+	},
+})
 
 export default GlobalStyles
